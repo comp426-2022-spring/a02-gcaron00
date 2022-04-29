2e55873b7c2f5eaeb3e74b6a11b9bd136c8d6334
gcaron00 <gcaron@live.unc.edu>
2022-04-29 11:03:43 -0400

    fix edge cases

--- guess-flip.js
+++ guess-flip.js
@@ -2,7 +2,7 @@ import * as myModule from './modules/coin.mjs';
 import minimist from "minimist"
 
 const args = minimist(process.argv.slice(2));
-//const call = args.call || console.error("Error: no input. \nUsage: node guess-flip --call=[heads|tails]"); process.exit(1)
+const call = args.call // || console.error("Error: no input. \nUsage: node guess-flip --call=[heads|tails]"); process.exit(1)
 if(call == null || (call !== "heads" && call !== "tails")){
     console.error("Error: no input. \nUsage: node guess-flip --call=[heads|tails]"); 
     process.exit(1)
